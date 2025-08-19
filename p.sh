2f9278aba83f7249d4801d9e2f048cbe3d27ea56
雪桃 <60215664+LoranaAurelia@users.noreply.github.com>
2025-08-19 23:04:33 +1000

    Update p.sh

--- p.sh
+++ p.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # ======= 配置 / Config =======
-BASE="http://10.0.10.136:8008"   # ← 改这里即可 / Change me only
+BASE="https://raw.githubusercontent.com/LoranaAurelia/Lorana-s-Debian-sh/main"   # ← 改这里即可 / Change me only
 SSH_SCRIPT="ssh.sh"
 SRC_SCRIPT="src.sh"
 CN_SCRIPT="cn.sh"
